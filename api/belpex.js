e5746ea2dbb5f122d83c9d1963c8e56bb97b9943
COSTAVE <glacis-wiegen3d@icloud.com>
2025-06-20 18:23:04 +0200

    Update belpex.js

--- api/belpex.js
+++ api/belpex.js
@@ -1,15 +1,20 @@
-import * as Epex from '@tvanlaerhoven/epex-client';
+import * as Epex from 'https://esm.sh/@tvanlaerhoven/epex-client';
 
 export default async function handler(req, res) {
-  const client = new Epex.Client();
-  const data = await client.getDayAheadMarketData(
-    Epex.MarketArea.Belgium,
-    Epex.today()
-  );
-  const result = data.entries.map(e => ({
-    hour: `${e.hour}:00`,
-    price: (e.price * 100).toFixed(2)
-  }));
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json(result);
+  try {
+    const client = new Epex.Client();
+    const data = await client.getDayAheadMarketData(
+      Epex.MarketArea.Belgium,
+      Epex.today()
+    );
+    const result = data.entries.map(e => ({
+      hour: `${e.hour}:00`,
+      price: (e.price * 100).toFixed(2)
+    }));
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
